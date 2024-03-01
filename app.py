56df39740944092dddb90e230e623d42f1f638dd
HaXr <haxr@outlook.dk>
2024-03-01 12:27:07 +0100

    matplotlib & stuff
    Nået til 38:42 i videoen:
    https://kea.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=5e24b814-0938-4fcd-bc51-b11f00861b41

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask, render_template
+import base64
+from io import BytesIO
+from matplotlib.figure import Figure
 
 app = Flask(__name__)
+
+
+def stue_temp():
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([1, 2])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/stue')
+def stue():
+    stue_temperature = stue_temp()
+    return render_template('stue.html', stue_temperature = stue_temperature)
 
 app.run(debug=True)
